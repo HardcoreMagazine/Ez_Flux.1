83da9f59c40e385f2e228d1e642bd2ed964d97d4
HardcoreMagazine <hardcoremagazine@users.noreply.github.com>
2024-09-03 00:26:22 +0300

    fixed a typo

--- program/config/available_models.py
+++ program/config/available_models.py
@@ -1,7 +1,7 @@
 available_models = {
     1: {
         'name': 'black-forest-labs/FLUX.1-schnell',
-        'desc': 'Fast general-purpose model, licensed under Apache 2.0 agreement',
+        'desc': 'Fast general-purpose model, licensed under Apache 2.0 agreement.',
     },
     2: {
         'name': 'black-forest-labs/FLUX.1-dev',
